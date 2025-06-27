bf102a0f72060db81a87350c6b87c19e797a170d
ANKeshri <ankeshri2003@gmail.com>
2025-06-27 20:32:52 +0530

    hyper parameter

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -18,19 +19,24 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
-        for i in range(len(models)):
+        for key in models.keys():
+            model = models[key]
+            para = param.get(key, {})  # Use .get() to avoid KeyError, default to empty dict if not found
 
-            model = list(models.values())[i]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
-            model.fit(X_train, y_train) #training model
-            y_train_pred = model.predict(X_train) #predicting train data
-            y_test_pred = model.predict(X_test) #predicting test data
-            train_model_score = r2_score(y_train, y_train_pred) #calculating r2 score for train data
-            test_model_score = r2_score(y_test, y_test_pred) #calculating r2 score for test data
-            report[list(models.keys())[i]] = test_model_score
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)  # predicting train data
+            y_test_pred = model.predict(X_test)  # predicting test data
+            train_model_score = r2_score(y_train, y_train_pred)  # calculating r2 score for train data
+            test_model_score = r2_score(y_test, y_test_pred)  # calculating r2 score for test data
+            report[key] = test_model_score
         return report
     except Exception as e:
         raise CustomException(e, sys)
